0ef073460bd14e5eca5b02b95fde3ec6172449e6
Madhur Dixit <dmadhur6@gmail.com>
2020-09-18 02:48:03 +0530

    Basic Arithmetic

---
+++ 5. Basic Arithmetic/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int sum = 6+5; // Sum 
+    int dif = 6-5; // Difference
+    int prod = 6*5; // Product
+    int div = 6/5; // Division
+    int mod = 6%5; // Modulus(Remainder)
+
+    cout << sum << "," << dif << "," << prod << "," << div << "," << mod << endl;
+
+    return 0;
+}
