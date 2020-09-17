fe2291980b4f4c2bdc5ef1571d1208cdb7787306
Madhur Dixit <dmadhur6@gmail.com>
2020-09-18 03:07:37 +0530

    Basic If Statement

---
+++ 6. Basic If Statement/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cin >> a;
+    cin >> b;
+    
+    if(a>b){
+        cout << "a is greater than b";
+    }else
+    {
+        cout << "b is greater than a";
+    }
+    
+
+    return 0;
+}
