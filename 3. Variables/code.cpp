0220fca4ae119d8f57b80c83116f0b253528a617
Madhur Dixit <dmadhur6@gmail.com>
2020-09-18 01:45:56 +0530

    Variables

---
+++ 3. Variables/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// Variables are just placeholders
+int main(){
+
+    int var1 = 7; // Int Datatype takes integer
+    int var2 = 4;
+
+    int sum = var1 + var2;
+
+    cout << var1 << endl;
+    cout << var2 << endl;
+    cout << sum << endl;
+
+    return(0);
+
+}
