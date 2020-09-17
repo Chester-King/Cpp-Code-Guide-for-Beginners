fc9562c47aeb8df181764521d43e94ddb05b7d6b
Madhur Dixit <dmadhur6@gmail.com>
2020-09-18 01:34:42 +0530

    Basic Printing

---
+++ 2. Basic Printing/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Printing in same Line
+    cout << "Ah Yes ";
+    cout << "The negotiator";
+
+    cout << endl; // Changing line
+    // Printing in different Lines
+    cout << "Ah Yes " << endl;
+    cout << "The negotiator";
+
+    cout << endl; // Changing line
+    // Printing in different Lines using escape character
+    cout << "Ah Yes \n";
+    cout << "The negotiator";
+}
