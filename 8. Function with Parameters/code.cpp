76bd97d177270eff16f5e76bae7a18d18127d973
Madhur Dixit <dmadhur6@gmail.com>
2020-09-20 09:57:11 +0530

    Function with parameters

---
+++ 8. Function with Parameters/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int addition(int a,int b){
+
+    int c= a+b;
+    return c;
+
+}
+
+int main(){
+
+    cout << addition(3,6);
+
+    return 0;
+
+}
+
