e770ade933e4cc517c7c8198e2f88965560a6b31
Madhur Dixit <dmadhur6@gmail.com>
2020-09-23 09:33:20 +0530

    For Loop

---
+++ 13. For Loop/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n=0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << endl;
+    }
+    
+
+    return 0;
+}
