f60c1c59cd76d13ac10b09accac23a2466b3dda8
Madhur Dixit <dmadhur6@gmail.com>
2020-09-25 05:26:38 +0530

    Putting Values in Array

---
+++ 17. Putting Values in Array/code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int ary[7];
+
+    for (int i = 0; i < 7; i++)
+    {
+        ary[i]=i;
+    }
+    for (int i = 0; i < 7; i++)
+    {
+        cout << ary[i] << endl;
+    }
+
+    
+    
+
+    return 0;
+}
