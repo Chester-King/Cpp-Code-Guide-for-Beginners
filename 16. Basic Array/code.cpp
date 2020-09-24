37790acca830fabd64c99be35d5c519357a205ce
Madhur Dixit <dmadhur6@gmail.com>
2020-09-25 05:05:10 +0530

    Basic Array

---
+++ 16. Basic Array/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int ary[5] = {3,6,9,2,1};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << ary[i] << endl;
+    }
+    
+
+    return 0;
+
+}
